7b932f589917629fbc1c50aeb367b70c51cf2b02
Daniel González Lopes <danielgonzalezlopes@gmail.com>
2021-03-21 14:33:35 +0100

    Add example
    Signed-off-by: Daniel González Lopes <danielgonzalezlopes@gmail.com>

---
+++ examples/test-config.js
@@ -0,0 +1,17 @@
+import tracing, { Http } from 'k6/x/tracing';
+import { sleep } from 'k6';
+
+export function setup() {
+  console.log(`Running xk6-distributed-tracing v${tracing.version}`, tracing)
+}
+
+export default function() {
+  const http = new Http({
+    exporter: "jaeger",
+    propagator: "w3c",
+  });
+  const r = http.get('https://test-api.k6.io');
+  
+  console.log(JSON.stringify(r.request.headers))
+  sleep(1)
+}
