b9c491aad0c461ba18d614533f8ea244f9ed24ee
Daniel González Lopes <danielgonzalezlopes@gmail.com>
2021-03-21 14:34:50 +0100

    Add options to examples
    Signed-off-by: Daniel González Lopes <danielgonzalezlopes@gmail.com>

--- examples/test-config.js
+++ examples/test-config.js
@@ -1,6 +1,11 @@
 import tracing, { Http } from 'k6/x/tracing';
 import { sleep } from 'k6';
 
+export let options = {
+  vus: 1,
+  duration: '10s',
+};
+
 export function setup() {
   console.log(`Running xk6-distributed-tracing v${tracing.version}`, tracing)
 }
