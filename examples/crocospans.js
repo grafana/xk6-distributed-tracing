ab40d3be974cf6a2fb5638ecfec1b70c6e29f832
Daniel González Lopes <danielgonzalezlopes@gmail.com>
2022-09-26 20:18:51 +0200

    Add scenarios to demo
    Signed-off-by: Daniel González Lopes <danielgonzalezlopes@gmail.com>

--- examples/crocospans.js
+++ examples/crocospans.js
@@ -2,10 +2,24 @@ import { Http } from 'k6/x/tracing';
 import { check, group, sleep } from 'k6';
 
 export const options = {
-  stages: [
-    { duration: '20s', target: 20 },
-    { duration: '10s', target: 0 },
-  ],
+  scenarios: {
+    smokeDispatch: {
+      exec: 'dispatch',
+      executor: 'constant-vus',
+      vus: 1,
+      duration: '15s',
+    },
+    stressDispatch: {
+      exec: 'dispatch',
+      executor: 'ramping-vus',
+      stages: [
+        { duration: '25s', target: 20 },
+        { duration: '10s', target: 0 },
+      ],
+      gracefulRampDown: '5s',
+      startTime: '15s',
+    },
+  },
   ext: {
     loadimpact: {
       projectID: 3602528,
@@ -15,15 +29,18 @@ export const options = {
   }
 };
 
-export default function () {
-  const http = new Http({"endpoint": "http://localhost:10001/api/v1/push/request"});
+const tracingConfig = {"endpoint": "http://localhost:10001/api/v1/push/request"}
 
-  group('do something inside a group', function () {
-    let r = http.get('http://localhost:8080/dispatch?customer=123&nonse=0.41418777006108365');
+const userIDs = ["123", "392", "731", "567"]
+
+export function dispatch() {
+  const http = new Http(tracingConfig);
+  group('dispatch customer 123', function () {
+    let r = http.get(`http://localhost:8080/dispatch?customer=${userIDs[0]}`);
     check(r, {
       'status is 200': (r) => r.status === 200,
     });
-    console.log(`traceId=${r.trace_id}`);
-    sleep(1);
+    console.log(`trace_id=${r.trace_id} status=${r.status} method=${r.request.method} duration=${r.timings.duration}`)
   });
+  sleep(1);
 }
